ea0ac7066469225cf4a54a92aa872caecbeb4d29
Oscar Santos <oscar303data@gmail.com>
2022-11-10 06:31:55 -0700

    - block test add players to roster pass the test creating the method add_player.

--- lib/team.rb
+++ lib/team.rb
@@ -1,9 +1,18 @@
 class Team
     attr_reader :team_1,
-                :team_2
+                :team_2,
+                :roster,
+                :player_count
 
     def initialize(team_1, team_2)
         @team_1 = team_1
         @team_2 = team_2
+        @roster = []
+        @player_count = 0
+    end
+
+    def add_player(player)
+        @roster << player
+        @player_count = @roster.size
     end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -13,5 +13,20 @@ RSpec.describe Team do
         
         expect(team.team_1).to eq("Dodgers")
         expect(team.team_2).to eq("Los Angeles")
+        expect(team.roster).to eq([])
+        expect(team.player_count).to eq(0)
+    end
+
+    it 'can add players to roster' do
+        team = Team.new("Dodgers", "Los Angeles")
+        
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+
+        expect(team.roster).to eq([player_1, player_2])
+        expect(team.player_count).to eq(2)
     end
 end
