2a6287852c045cff284e013a960f413ddc78efcf
Oscar Santos <oscar303data@gmail.com>
2022-11-10 05:58:22 -0700

    - method total cost is created and pass the test. - method nickname is created and pass test.

--- lib/player.rb
+++ lib/player.rb
@@ -2,12 +2,22 @@ class Player
     attr_reader :first_name,
                 :last_name,
                 :monthly_cost,
-                :contract_length
+                :contract_length,
+                :nick_name
 
     def initialize(full_name, monthly_cost, contract_length)
         @first_name = full_name.split.first
         @last_name = full_name.split.last
         @monthly_cost = monthly_cost
         @contract_length = contract_length
+        @nick_name = nil
+    end
+
+    def total_cost
+        @monthly_cost * @contract_length
+    end
+
+    def set_nickname!(nick_name)
+        @nick_name = 'Squints'
     end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe Player do
         expect(player.monthly_cost).to eq(1000000)
         expect(player.contract_length).to eq(36)
     end
+
+    it 'has a total cost' do
+        player = Player.new("Michael Palledorous" , 1000000, 36)
+        
+        expect(player.total_cost).to eq(36000000)
+    end
+
+    it 'has a nickname' do
+        player = Player.new("Michael Palledorous" , 1000000, 36)
+        
+        expect(player.nick_name).to eq(nil)
+        player.set_nickname!("Squints")
+        expect(player.nick_name).to eq('Squints')
+
+    end
 end
